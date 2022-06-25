9255570a87fe6f9844467ef997482b5d747a53de
kazi <champlonremii@gmail.com>
2022-06-25 16:49:47 +0200

    basic random

--- back/controllers/slotMachine.js
+++ back/controllers/slotMachine.js
@@ -1,5 +1,24 @@
 //const SlotMachine = require('../models/slotMachine');
 
 exports.result = (req, res, next) => {
-    res.send('Successful response to slot machine result');
+
+    let array = [1, 2, 3];
+    let resultArray = [];
+
+
+    for (let i = 0; i < array.length; i++ )
+    {
+        let rand = Math.random();
+        rand *= array.length;
+        rand = Math.floor(rand) + 1;
+        resultArray.push(rand);
+    }
+
+    let winning = resultArray.every( (val, i, arr) => val === arr[0] )
+
+    let result = {
+        result: resultArray,
+        wining: winning
+    }
+    res.status(200).json(result);
 };
